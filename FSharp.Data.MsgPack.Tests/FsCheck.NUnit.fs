4fa4abde1164ffa9d54933657547d31eda6ea7a6
pocketberserker <pocketberserker@gmail.com>
2014-03-15 16:12:14 +0900

    use FsCheck

---
+++ FSharp.Data.MsgPack.Tests/FsCheck.NUnit.fs
@@ -0,0 +1,62 @@
+﻿module FsCheck.NUnit
+
+open FsCheck
+open NUnit.Framework
+
+let runner =
+  { new IRunner with
+      member x.OnStartFixture t = ()
+      member x.OnArguments(ntets: int, args: obj list, every: int -> obj list -> string) = ()
+      member x.OnShrink(args, everyShrink) = ()
+      member x.OnFinished(name, result) =
+        match result with
+        | TestResult.True _ -> ()
+        | _ -> Assert.Fail(Runner.onFinishedToString name result) }
+
+let config = { Config.Default with Runner = runner }
+
+let check testable =
+  Check.One ("", config, testable)
+
+open System
+
+// copy from https://github.com/fsharp/FsCheck/blob/1628773ed742fa223900fb706e44b20c3bd04db7/FsCheck/Arbitrary.fs
+// TODO: upgrade FsCheck if new version of FsCheck is released.
+type DefaultExtension =
+
+  static member UInt16() =
+    Arb.from<int>
+    |> Arb.convert (abs >> uint16) int
+
+  static member UInt32() =
+    Arb.from<int>
+    |> Arb.convert (abs >> uint32) int
+
+  static member UInt64() =
+    Arb.from<int>
+    |> Arb.convert (abs >> uint64) int
+
+  static member private fraction (a:int) (b:int) (c:int) = 
+    double a + double b / (abs (double c) + 1.0)
+
+  static member Float32() = 
+    { new Arbitrary<float32>() with
+      override x.Generator = 
+        let fraction a b c = float32 (DefaultExtension.fraction a b c)
+        Gen.frequency [
+          (6, Gen.map3 fraction Arb.generate Arb.generate Arb.generate)
+          (1, Gen.elements [ Single.NaN; Single.NegativeInfinity; Single.PositiveInfinity])
+          (1, Gen.elements [ Single.MaxValue; Single.MinValue; Single.Epsilon])]
+      override x.Shrinker fl =
+        let (|<|) x y = abs x < abs y
+        seq {
+          if Single.IsInfinity fl || Single.IsNaN fl then 
+            yield 0.0f
+          else
+            if fl < 0.0f then yield -fl
+            let truncated = truncate fl
+            if truncated |<| fl then yield truncated }
+        |> Seq.distinct
+    }
+
+do Arb.register<DefaultExtension>() |> ignore
--- FSharp.Data.MsgPack.Tests/MsgPackTest.fs
+++ FSharp.Data.MsgPack.Tests/MsgPackTest.fs
@@ -2,6 +2,8 @@
 
 open NUnit.Framework
 open FsUnit
+open FsCheck
+open FsCheck.NUnit
 open MsgPackValue
 
 [<TestFixture>]
@@ -10,183 +12,119 @@ module MsgPackTest =
   open Limited
 
   [<Test>]
-  let ``unpack nil`` () =
-    [| 0xc0uy |]
-    |> MsgPack.unpack |> should equal (Some Nil)
+  let ``nil`` () =
+    Nil
+    |> MsgPack.pack
+    |> MsgPack.unpack
+    |> should equal (Some Nil)
   
   [<Test>]
-  let ``unpack false`` () =
-    [| 0xc2uy |]
-    |> MsgPack.unpack |> should equal (Some False)
+  let ``false`` () =
+    False
+    |> MsgPack.pack
+    |> MsgPack.unpack
+    |> should equal (Some False)
 
   [<Test>]
-  let ``unpack true`` () =
-    [| 0xc3uy |]
-    |> MsgPack.unpack |> should equal (Some True)
+  let ``true`` () =
+    True
+    |> MsgPack.pack
+    |> MsgPack.unpack
+    |> should equal (Some True)
 
   [<Test>]
-  let ``unpack positive fix int`` () =
-    [| 0x7fuy |]
-    |> MsgPack.unpack |> should equal (Some (UInt8 127uy))
+  let ``uint8`` () =
+    check <| fun x ->
+      let value = UInt8 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack uint8`` () =
-    [| 0xccuy; 0xffuy |]
-    |> MsgPack.unpack |> should equal (Some (UInt8 255uy))
+  let ``uint16`` () =
+    check <| fun x ->
+      let value = UInt16 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack uint16`` () =
-    [| 0xcduy; 0xffuy; 0xffuy |]
-    |> MsgPack.unpack |> should equal (Some (UInt16 System.UInt16.MaxValue))
+  let ``uint32`` () =
+    check <| fun x ->
+      let value = UInt32 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack uint32`` () =
-    [| 0xceuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy |]
-    |> MsgPack.unpack |> should equal (Some (UInt32 System.UInt32.MaxValue))
+  let ``uint64`` () =
+    check <| fun x ->
+      let value = UInt64 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack uint64`` () =
-    [| 0xcfuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy |]
-    |> MsgPack.unpack |> should equal (Some (UInt64 System.UInt64.MaxValue))
+  let ``int8`` () =
+    check <| fun x ->
+      let value = Int8 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack negative fix int`` () =
-    [| 0xffuy |]
-    |> MsgPack.unpack |> should equal (Some (Int8 31y))
+  let ``int16`` () =
+    check <| fun x ->
+      let value = Int16 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack int8`` () =
-    [| 0xd0uy; 0x7fuy |]
-    |> MsgPack.unpack |> should equal (Some (Int8 System.SByte.MaxValue))
+  let ``int32`` () =
+    check <| fun x ->
+      let value = Int32 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack int16`` () =
-    [| 0xd1uy; 0xffuy; 0x7fuy |]
-    |> MsgPack.unpack |> should equal (Some (Int16 System.Int16.MaxValue))
+  let ``int64`` () =
+    check <| fun x ->
+      let value = Int64 x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack int32`` () =
-    [| 0xd2uy; 0xffuy; 0xffuy; 0xffuy; 0x7fuy |]
-    |> MsgPack.unpack |> should equal (Some (Int32 System.Int32.MaxValue))
+  let ``float32`` () =
+    check <| fun x ->
+      let value = Float32 x
+      match value |> MsgPack.pack |> MsgPack.unpack with
+      | Some (Float32 actual) when System.Single.IsNaN(actual) -> true
+      | Some (Float32 actual) -> actual = x
+      | _ -> false
 
   [<Test>]
-  let ``unpack int64`` () =
-    [| 0xd3uy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0x7fuy |]
-    |> MsgPack.unpack |> should equal (Some (Int64 System.Int64.MaxValue))
+  let ``float64`` () =
+    check <| fun x ->
+      let value = Float64 x
+      match value |> MsgPack.pack |> MsgPack.unpack with
+      | Some (Float64 actual) when System.Double.IsNaN(actual) -> true
+      | Some (Float64 actual) -> actual = x
+      | _ -> false
 
   [<Test>]
-  let ``unpack float32`` () =
-    [| 0xcauy; 0xffuy; 0xffuy; 0x7fuy; 0x7fuy |]
-    |> MsgPack.unpack |> should equal (Some (Float32 System.Single.MaxValue))
+  let ``string`` () =
+    check <| fun x ->
+      let value = String x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack float64`` () =
-    [| 0xcbuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xefuy; 0x7fuy |]
-    |> MsgPack.unpack |> should equal (Some (Float64 System.Double.MaxValue))
-
-  let genString n str = System.String(str, n)
-  let toBytes s = Microsoft.FSharp.Collections.Array.map byte s
+  let ``binary`` () =
+    check <| fun x ->
+      let value = Binary x
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack fix string`` () =
-    let expected = genString 31 'A'
-    [| yield 0xbfuy; yield! System.Text.Encoding.UTF8.GetBytes(expected) |]
-    |> MsgPack.unpack |> should equal (Some (String expected))
+  let ``array`` () =
+    check <| fun xs ->
+      let value =
+        xs
+        |> Microsoft.FSharp.Collections.Array.map String
+        |> Array
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
 
   [<Test>]
-  let ``unpack string 8`` () =
-    let expected = genString 255 'A'
-    [| yield 0xd9uy; yield 0xffuy; yield! System.Text.Encoding.UTF8.GetBytes(expected) |]
-    |> MsgPack.unpack |> should equal (Some (String expected))
-
-  [<Test>]
-  let ``unpack string 16`` () =
-    let length = 1000us
-    let expected = genString (int length) 'A'
-    [|
-      yield 0xdauy
-      yield! System.BitConverter.GetBytes(length)
-      yield! System.Text.Encoding.UTF8.GetBytes(expected) |]
-    |> MsgPack.unpack |> should equal (Some (String expected))
-
-  [<Test>]
-  let ``unpack string 32`` () =
-    let length = 70000u
-    let expected = genString (int length) 'A'
-    [|
-      yield 0xdbuy
-      yield! System.BitConverter.GetBytes(length)
-      yield! System.Text.Encoding.UTF8.GetBytes(expected) |]
-    |> MsgPack.unpack |> should equal (Some (String expected))
-
-  let genBin n = Microsoft.FSharp.Collections.Array.create n 0xffuy
-
-  [<Test>]
-  let ``unpack binary 8`` () =
-    let expected = genBin 255
-    [| yield 0xc4uy; yield 0xffuy; yield! expected |]
-    |> MsgPack.unpack |> should equal (Some (Binary expected))
-
-  [<Test>]
-  let ``unpack binary 16`` () =
-    let length = 1000us
-    let expected = genBin (int length)
-    [|
-      yield 0xc5uy
-      yield! System.BitConverter.GetBytes(length)
-      yield! expected |]
-    |> MsgPack.unpack |> should equal (Some (Binary expected))
-
-  [<Test>]
-  let ``unpack binary 32`` () =
-    let length = 70000u
-    let expected = genBin (int length)
-    [|
-      yield 0xc6uy
-      yield! System.BitConverter.GetBytes(length)
-      yield! expected |]
-    |> MsgPack.unpack |> should equal (Some (Binary expected))
-
-  [<Test>]
-  let ``unpack fix array`` () =
-    let expected = [| UInt8 1uy; UInt8 2uy; UInt8 3uy |]
-    [| 0x93uy; 0x01uy; 0x02uy; 0x03uy |]
-    |> MsgPack.unpack |> should equal (Some (Array expected))
-
-  [<Test>]
-  let ``unpack array16`` () =
-    let length = 16
-    let input = Microsoft.FSharp.Collections.Array.create length 0uy
-    let expected = Microsoft.FSharp.Collections.Array.map UInt8 input
-    [| yield 0xdcuy; yield! System.BitConverter.GetBytes(length); yield! input |]
-    |> MsgPack.unpack |> should equal (Some (Array expected))
-
-  [<Test>]
-  let ``unpack fix map`` () =
-    let expected = [ (String "compact", True); (String "schema", UInt8 0uy) ] |> Microsoft.FSharp.Collections.Map.ofList
-    [|
-      yield 0x82uy
-      yield 0xa7uy
-      yield! System.Text.Encoding.UTF8.GetBytes("compact")
-      yield 0xc3uy
-      yield 0xa6uy
-      yield! System.Text.Encoding.UTF8.GetBytes("schema")
-      yield 0x00uy |]
-    |> MsgPack.unpack |> should equal (Some (Map expected))
-
-
-  [<Test>]
-  let ``unpack map 16`` () =
-    let length = 16
-    let data = [ for _ in 0 .. length do yield ("schema", True) ]
-    let expected = data |> Microsoft.FSharp.Collections.List.map (fun (x,y) -> (String x, y)) |> Microsoft.FSharp.Collections.Map.ofList
-    [|
-      yield 0xdeuy
-      yield! System.BitConverter.GetBytes(uint16 length)
-      yield! data |> Microsoft.FSharp.Collections.Seq.collect (fun (x,y) ->
-        [|
-          yield 0xa6uy
-          yield! System.Text.Encoding.UTF8.GetBytes("schema")
-          yield 0xc3uy |])
-    |]
-    |> MsgPack.unpack |> should equal (Some (Map expected))
+  let ``map`` () =
+    check <| fun (xs: (string * string) list) ->
+      let value =
+        xs
+        |> Microsoft.FSharp.Collections.List.map (fun (k,v) -> (String k, String v))
+        |> Microsoft.FSharp.Collections.Map.ofList
+        |> Map
+      Some value = (value |> MsgPack.pack |> MsgPack.unpack)
